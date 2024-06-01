073935fd313dd0244ab454dec643045ab76a52e8
li-guohao <git@liguohao.cn>
2024-06-02 01:41:36 +0800

    feat: finsih produce inventory manager

--- Program.cs
+++ Program.cs
@@ -22,62 +22,341 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
+        /*
+        * R e a d m e
+        * -----------
+        * Production equipment inventory management for LCD.
+        * 太空工程师，生产设备库存管理脚本， 自动将装配机精炼厂的产物，放进储物箱子。
+        * 
+        * @see <https://github.com/se-scripts/produce-inventory-manager>
+        * @author [Hi.James](https://space.bilibili.com/368005035)
+        * @author [li-guohao](https://github.com/li-guohao)
+        */
+        MyIni _ini = new MyIni();
+
+        List<IMyCargoContainer> cargoContainers = new List<IMyCargoContainer>();
+        List<IMyAssembler> assemblers = new List<IMyAssembler>();
+        List<IMyRefinery> refineries = new List<IMyRefinery>();
+
+        Color background_Color = new Color(0, 35, 45);
+        Color border_Color = new Color(0, 130, 255);
+
+        int counter_InventoryManagement = 0, counter_AssemblerManagement = 0, counter_RefineryManagement = 0;
+        double counter_Logo = 0;
+
+        public struct ItemList
+        {
+            public string Name;
+            public double Amount;
+        }
+        ItemList[] itemList_All;
+        ItemList[] itemList_Ore;
+        ItemList[] itemList_Ingot;
+        ItemList[] itemList_Component;
+        ItemList[] itemList_AmmoMagazine;
+
+
+        public struct ComparisonTable
+        {
+            public string Name;
+            public string BluePrintName;
+            public double Amount;
+            public bool HasItem;
+        }
 
         public Program()
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set Runtime.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+
+            GridTerminalSystem.GetBlocksOfType(cargoContainers, b => b.IsSameConstructAs(Me));
+            GridTerminalSystem.GetBlocksOfType(assemblers, b => b.IsSameConstructAs(Me));
+            GridTerminalSystem.GetBlocksOfType(refineries, b => b.IsSameConstructAs(Me) && !b.BlockDefinition.ToString().Contains("Shield"));
+
+            ProgrammableBlockScreen();
         }
 
-        public void Save()
+        public void DebugLCD(string text)
         {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
+            List<IMyTextPanel> debugPanel = new List<IMyTextPanel>();
+            GridTerminalSystem.GetBlocksOfType(debugPanel, b => b.IsSameConstructAs(Me) && b.CustomName == "DEBUGLCD");
+
+            if (debugPanel.Count == 0) return;
+
+            string temp = "";
+            foreach (var panel in debugPanel)
+            {
+                temp = "";
+                temp = panel.GetText();
+            }
+
+            foreach (var panel in debugPanel)
+            {
+                if (panel.ContentType != ContentType.TEXT_AND_IMAGE) panel.ContentType = ContentType.TEXT_AND_IMAGE;
+                panel.FontSize = 0.55f;
+                panel.Font = "LoadingScreen";
+                panel.WriteText(DateTime.Now.ToString(), false);
+                panel.WriteText("\n", true);
+                panel.WriteText(text, true);
+                panel.WriteText("\n", true);
+                panel.WriteText(temp, true);
+            }
+        }
+
+        public void Assembler_to_CargoContainers()
+        {
+            Echo("Assembler_to_CargoContainers");
+
+            for (int i = 0; i < 10; i++)
+            {
+                int assemblerCounter = counter_AssemblerManagement * 10 + i;
+
+                if (assemblerCounter >= assemblers.Count)
+                {
+                    counter_AssemblerManagement = 0;
+                    return;
+                }
+                else
+                {
+                    var assembler = assemblers[assemblerCounter];
+                    double currentVolume_Double = ((double)assembler.OutputInventory.CurrentVolume);
+                    double maxVolume_Double = ((double)assembler.OutputInventory.MaxVolume);
+                    double volumeRatio_Double = currentVolume_Double / maxVolume_Double;
+
+                    if (assemblers[assemblerCounter].IsProducing == false)
+                    {
+                        foreach (var cargoContainer in cargoContainers)
+                        {
+                            List<MyInventoryItem> items1 = new List<MyInventoryItem>();
+                            assemblers[assemblerCounter].InputInventory.GetItems(items1);
+                            foreach (var item in items1)
+                            {
+                                bool tf = assemblers[assemblerCounter].InputInventory.TransferItemTo(cargoContainer.GetInventory(), item);
+                            }
+
+                            List<MyInventoryItem> items2 = new List<MyInventoryItem>();
+                            assemblers[assemblerCounter].OutputInventory.GetItems(items2);
+                            foreach (var item in items2)
+                            {
+                                bool tf = assemblers[assemblerCounter].OutputInventory.TransferItemTo(cargoContainer.GetInventory(), item);
+                            }
+
+                            items1.Clear();
+                            items2.Clear();
+                            assemblers[assemblerCounter].InputInventory.GetItems(items1);
+                            assemblers[assemblerCounter].OutputInventory.GetItems(items2);
+                            if (items1.Count < 1 && items2.Count == 0) break;
+                        }
+                    }
+                    else
+                    {
+                        if (volumeRatio_Double > 0)
+                        {
+                            foreach (var cargoContainer in cargoContainers)
+                            {
+                                List<MyInventoryItem> items3 = new List<MyInventoryItem>();
+                                assembler.OutputInventory.GetItems(items3);
+                                foreach (var item in items3)
+                                {
+                                    bool tf = assembler.OutputInventory.TransferItemTo(cargoContainer.GetInventory(), item);
+                                }
+                                items3.Clear();
+                                assembler.OutputInventory.GetItems(items3);
+                                if (items3.Count < 1) break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            counter_AssemblerManagement++;
+        }// Assembler_to_CargoContainers END!
+
+        public void ShowProductionQueue()
+        {
+            StringBuilder str = new StringBuilder();
+            foreach (var assembler in assemblers)
+            {
+                List<MyProductionItem> productionitems = new List<MyProductionItem>();
+                assembler.GetQueue(productionitems);
+                foreach (var item1 in productionitems)
+                {
+                    str.Append($"{assembler.CustomName}:{item1.BlueprintId}:{item1.Amount}");
+                    str.Append("\n");
+                }
+            }
+            DebugLCD(str.ToString());
+        }
+
+        public void Refinery_to_CargoContainers()
+        {
+            Echo("Refinery_to_CargoContainers");
+            for (int i = 0; i < 10; i++)
+            {
+                int refineryCounter = counter_RefineryManagement * 10 + i;
+
+                if (refineryCounter >= refineries.Count)
+                {
+                    counter_RefineryManagement = 0;
+                    return;
+                }
+                else
+                {
+                    if (refineries[refineryCounter].IsProducing == false)
+                    {
+                        foreach (var cargoContainer in cargoContainers)
+                        {
+                            List<MyInventoryItem> items1 = new List<MyInventoryItem>();
+                            refineries[refineryCounter].InputInventory.GetItems(items1);
+                            foreach (var item in items1)
+                            {
+                                bool tf = refineries[refineryCounter].InputInventory.TransferItemTo(cargoContainer.GetInventory(), item);
+                            }
+
+                            List<MyInventoryItem> items2 = new List<MyInventoryItem>();
+                            refineries[refineryCounter].OutputInventory.GetItems(items2);
+                            foreach (var item in items2)
+                            {
+                                bool tf = refineries[refineryCounter].OutputInventory.TransferItemTo(cargoContainer.GetInventory(), item);
+                            }
+
+                            items1.Clear();
+                            items2.Clear();
+                            refineries[refineryCounter].InputInventory.GetItems(items1);
+                            refineries[refineryCounter].OutputInventory.GetItems(items2);
+                            if (items1.Count < 1 && items2.Count < 1) break;
+                        }
+                    }
+                    else
+                    {
+                        foreach (var cargoContainer in cargoContainers)
+                        {
+                            //List<MyInventoryItem> items1 = new List<MyInventoryItem>();
+                            //refineries[refineryCounter].InputInventory.GetItems(items1);
+                            //foreach (var item in items1)
+                            //{
+                            //    string str = item.Type.ToString();
+                            //    if (str.IndexOf("_Ore") != -1)
+                            //    {
+                            //        bool tf = refineries[refineryCounter].InputInventory.TransferItemTo(cargoContainer.GetInventory(), item);
+                            //    }
+                            //}
+
+                            List<MyInventoryItem> items2 = new List<MyInventoryItem>();
+                            refineries[refineryCounter].OutputInventory.GetItems(items2);
+                            foreach (var item in items2)
+                            {
+                                bool tf = refineries[refineryCounter].OutputInventory.TransferItemTo(cargoContainer.GetInventory(), item);
+                            }
+                            items2.Clear();
+                            refineries[refineryCounter].OutputInventory.GetItems(items2);
+                            if (items2.Count < 1) break;
+                        }
+                    }
+                }
+            }
+            counter_RefineryManagement++;
+        }// Refinery_to_CargoContainers END!
+
+        public void ProgrammableBlockScreen()
+        {
+
+            //  512 X 320
+            IMyTextSurface panel = Me.GetSurface(0);
+
+            if (panel == null) return;
+            panel.ContentType = ContentType.SCRIPT;
+
+            MySpriteDrawFrame frame = panel.DrawFrame();
+
+            float x = 512 / 2, y1 = 205;
+            DrawLogo(frame, x, y1, 200);
+            PanelWriteText(frame, "Production inventory management\nby Hi.James and li-guohao.", x, y1 + 110, 1f, TextAlignment.CENTER);
+
+            frame.Dispose();
+
+        }
+
+        public void PanelWriteText(MySpriteDrawFrame frame, string text, float x, float y, float fontSize, TextAlignment alignment)
+        {
+            MySprite sprite = new MySprite()
+            {
+                Type = SpriteType.TEXT,
+                Data = text,
+                Position = new Vector2(x, y),
+                RotationOrScale = fontSize,
+                Color = Color.Coral,
+                Alignment = alignment,
+                FontId = "LoadingScreen"
+            };
+            frame.Add(sprite);
+        }
+
+        public void DrawLogo(MySpriteDrawFrame frame, float x, float y, float width)
+        {
+            MySprite sprite = new MySprite()
+            {
+                Type = SpriteType.TEXTURE,
+                Data = "Screen_LoadingBar",
+                Position = new Vector2(x, y),
+                Size = new Vector2(width - 6, width - 6),
+                RotationOrScale = Convert.ToSingle(counter_Logo / 360 * 2 * Math.PI),
+                Alignment = TextAlignment.CENTER,
+            };
+            frame.Add(sprite);
+
+            sprite = new MySprite()
+            {
+                Type = SpriteType.TEXTURE,
+                Data = "Screen_LoadingBar",
+                Position = new Vector2(x, y),
+                Size = new Vector2(width / 2, width / 2),
+                RotationOrScale = Convert.ToSingle(2 * Math.PI - counter_Logo / 360 * 2 * Math.PI),
+                Alignment = TextAlignment.CENTER,
+            };
+            frame.Add(sprite);
+
+            sprite = new MySprite()
+            {
+                Type = SpriteType.TEXTURE,
+                Data = "Screen_LoadingBar",
+                Position = new Vector2(x, y),
+                Size = new Vector2(width / 4, width / 4),
+                RotationOrScale = Convert.ToSingle(Math.PI + counter_Logo / 360 * 2 * Math.PI),
+                Alignment = TextAlignment.CENTER,
+            };
+            frame.Add(sprite);
+
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
+            Echo($"{DateTime.Now}");
+            Echo("Program is running.");
+
+            DateTime beforDT = System.DateTime.Now;
+
+            if (counter_InventoryManagement++ >= 4) counter_InventoryManagement = 1;
+
+            switch (counter_InventoryManagement)
+            {
+                case 1:
+                    Assembler_to_CargoContainers();
+                    ShowProductionQueue();
+                    break;
+                case 2:
+                    Refinery_to_CargoContainers();
+                    break;
+                case 3:
+                    Assembler_to_CargoContainers();
+                    break;
+            }
+
+
+            DateTime afterDT = System.DateTime.Now;
+            TimeSpan ts = afterDT.Subtract(beforDT);
+            Echo("Total cost ms：" + ts.TotalMilliseconds);
+
+            DebugLCD("Total cost ms: " + ts.TotalMilliseconds);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # produce-inventory-manager
+Production equipment inventory management for LCD by Space Engineers Script.
+
+太空工程师，生产设备库存管理脚本， 自动将装配机精炼厂的产物，放进储物箱子。
+
+# 来源
+
+- [游戏《太空工程师》图形化物料显示界面](https://www.bilibili.com/read/cv27778300/)
+- <https://github.com/malware-dev/MDK-SE/wiki>
+
+# 作者
+
+- [Hi.James](https://space.bilibili.com/368005035)
+- [li-guohao](https://github.com/li-guohao)
