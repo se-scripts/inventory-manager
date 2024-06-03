e734dda1a447f8e81cfadc4b48ba1a450228d415
li-guohao <git@liguohao.cn>
2024-06-03 18:13:44 +0800

    style: add \n in pb block screen info.

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ namespace IngameScript
         * @author [Hi.James](https://space.bilibili.com/368005035)
         * @author [li-guohao](https://github.com/li-guohao)
         */
-        const string version = "1.0.0";
+        const string version = "1.0.1";
         MyIni _ini = new MyIni();
 
         List<IMyCargoContainer> cargoContainers = new List<IMyCargoContainer>();
@@ -163,7 +163,7 @@ namespace IngameScript
 
             float x = 512 / 2, y1 = 205;
             DrawLogo(frame, x, y1, 200);
-            PanelWriteText(frame, "Inventory management\nby Hi.James and li-guohao With version " + version, x, y1 + 110, 1f, TextAlignment.CENTER);
+            PanelWriteText(frame, "Inventory management\nby Hi.James and li-guohao\nWith version " + version, x, y1 + 110, 1f, TextAlignment.CENTER);
 
             frame.Dispose();
 
